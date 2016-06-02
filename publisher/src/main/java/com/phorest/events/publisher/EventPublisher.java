df09400eb54e008bcce779b05f16791eab2789eb
Paweł Malon <pawel.malon@phorest.com>
2016-06-02 12:07:44 +0200

    PH-936 Recommended Services - Regular Updates to Trend Data
    * Marked EventPublisher.java as a functional interface

--- publisher/src/main/java/com/phorest/events/publisher/EventPublisher.java
+++ publisher/src/main/java/com/phorest/events/publisher/EventPublisher.java
@@ -2,6 +2,7 @@ package com.phorest.events.publisher;
 
 import com.phorest.events.model.EventSource;
 
+@FunctionalInterface
 public interface EventPublisher {
 
     void publishEvent(String exchange, String routingKeyTemplate, EventSource eventSource);
