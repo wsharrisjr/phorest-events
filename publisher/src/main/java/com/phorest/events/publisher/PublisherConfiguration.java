85c79080844d2492c55f7e4e6523104835adad8d
Paweł Malon <pawel.malon@phorest.com>
2016-06-15 18:24:41 +0200

    * Disabled PublisherConfiguration.java for non amqp profile

--- publisher/src/main/java/com/phorest/events/publisher/PublisherConfiguration.java
+++ publisher/src/main/java/com/phorest/events/publisher/PublisherConfiguration.java
@@ -2,9 +2,11 @@ package com.phorest.events.publisher;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAspectJAutoProxy
+@Profile("amqp")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableAsync
 @Configuration
 public class PublisherConfiguration {
