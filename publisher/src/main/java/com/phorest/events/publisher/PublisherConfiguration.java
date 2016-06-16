4990b3ebd2bd37c980da203dea7540894a57aa14
Paweł Malon <pawel.malon@phorest.com>
2016-06-16 10:37:05 +0200

    * Removed AOP

--- publisher/src/main/java/com/phorest/events/publisher/PublisherConfiguration.java
+++ publisher/src/main/java/com/phorest/events/publisher/PublisherConfiguration.java
@@ -1,12 +1,10 @@
 package com.phorest.events.publisher;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Profile("amqp")
-@EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableAsync
 @Configuration
 public class PublisherConfiguration {
