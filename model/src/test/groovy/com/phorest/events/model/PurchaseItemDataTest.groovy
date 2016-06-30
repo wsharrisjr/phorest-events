7ce09b75f73c3c4d951d61febc9a8a7cba3929d1
Sarah Thornton <sarah.thornton@phorest.com>
2016-06-30 12:58:30 +0100

    PH-1869 [Server] - Receive purchase events and store * Added isProduct method on purchaseItem as is there for isService * Added unit test

--- model/src/test/groovy/com/phorest/events/model/PurchaseItemDataTest.groovy
+++ model/src/test/groovy/com/phorest/events/model/PurchaseItemDataTest.groovy
@@ -6,34 +6,54 @@ import spock.lang.Unroll
 
 class PurchaseItemDataTest extends Specification {
 
-  @Unroll
-  def "should deserialize json to expected type"() {
-    given:
-    ObjectMapper objectMapper = new ObjectMapper()
+    @Unroll
+    def "should deserialize json to expected type"() {
+        given:
+        ObjectMapper objectMapper = new ObjectMapper()
 
-    when:
-    PurchaseItemData result = objectMapper.readValue(json, PurchaseItemData)
+        when:
+        PurchaseItemData result = objectMapper.readValue(json, PurchaseItemData)
 
-    then:
-    result.class == expectedType
+        then:
+        result.class == expectedType
 
-    where:
-    json                          | expectedType
-    productPurchaseItemDataJson() | ProductPurchaseItemData
-    voucherPurchaseItemDataJson() | VoucherPurchaseItemData
-    otherPurchaseItemDataJson()   | PurchaseItemData
-  }
+        where:
+        json                          | expectedType
+        productPurchaseItemDataJson() | ProductPurchaseItemData
+        voucherPurchaseItemDataJson() | VoucherPurchaseItemData
+        otherPurchaseItemDataJson()   | PurchaseItemData
+    }
 
-  private static String productPurchaseItemDataJson() {
-    '{"itemType":"PRODUCT","productCategory":{"id":"category1"}}'
-  }
+    @Unroll
+    def "should return correct boolean value  for isX for #itemType"(ItemType itemType, boolean expectedIsService, boolean expectedIsProduct) {
+        given:
+        PurchaseItemData purchaseItemData = new PurchaseItemData(itemType: itemType)
 
-  private static String voucherPurchaseItemDataJson() {
-    '{"itemType":"VOUCHER","voucherData":{"id":"category1"}}'
-  }
+        when:
+        boolean actualIsService = purchaseItemData.isService()
+        boolean actualIsProduct = purchaseItemData.isProduct()
 
-  private static String otherPurchaseItemDataJson() {
-    '{"itemType":"UNKNOWN","name": "some item"}'
-  }
+        then:
+        expectedIsService == actualIsService
+        expectedIsProduct == actualIsProduct
+
+        where:
+        itemType         | expectedIsService | expectedIsProduct
+        ItemType.PRODUCT | false             | true
+        ItemType.SERVICE | true              | false
+        ItemType.UNKNOWN | false             | false
+    }
+
+    private static String productPurchaseItemDataJson() {
+        '{"itemType":"PRODUCT","productCategory":{"id":"category1"}}'
+    }
+
+    private static String voucherPurchaseItemDataJson() {
+        '{"itemType":"VOUCHER","voucherData":{"id":"category1"}}'
+    }
+
+    private static String otherPurchaseItemDataJson() {
+        '{"itemType":"UNKNOWN","name": "some item"}'
+    }
 
 }
