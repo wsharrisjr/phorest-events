e4e0b03faf256fcbd9fafbe6bffb21628c4f58e7
Paweł Malon <pawel.malon@phorest.com>
2016-10-12 13:13:54 +0200

    * Added new timestamp field to the MessageDeliveryFeedbackData

--- model/src/main/java/com/phorest/events/model/message/MessageDeliveryFeedbackData.java
+++ model/src/main/java/com/phorest/events/model/message/MessageDeliveryFeedbackData.java
@@ -5,6 +5,7 @@ import com.phorest.events.model.EventData;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.joda.time.DateTime;
 
 @Data
 @AllArgsConstructor(staticName = "of")
@@ -14,6 +15,7 @@ public class MessageDeliveryFeedbackData implements EventData {
     private String providedMessageId;
     private DeliveryStatus deliveryStatus;
     private MessageFailureData failure;
+    private DateTime timestamp;
 
     @JsonIgnore
     public boolean isFailed() {
